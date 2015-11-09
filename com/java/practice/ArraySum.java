5f6c9426afbe1bde36f6231317bc26057741ab31
Prajwal Halasahally KeshavaReddy <prajwal19@gmail.com>
2015-11-08 18:29:22 -0600

    array1

---
+++ com/java/practice/ArraySum.java
@@ -0,0 +1,30 @@
+package com.java.practice;
+import java.util.Scanner;
+
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
+
+public class ArraySum {
+
+	public static void main(String[] args) {
+		
+		int n;
+		int []arr;
+		
+		Scanner in = new Scanner(System.in);
+		
+		n=in.nextInt();
+		
+		arr=new int[n];
+		int sum=0;
+		for(int i=0; i<n; i++){
+			arr[i]=in.nextInt();
+			sum=sum+arr[i];
+			
+		}
+		System.out.println(sum);
+		
+
+	}
+
+}
