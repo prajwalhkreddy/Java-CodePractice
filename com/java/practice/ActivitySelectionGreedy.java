b85e9d3b8ada883989307c514611666b4b22257e
Prajwal Halasahally KeshavaReddy <prajwal19@gmail.com>
2015-11-09 23:28:48 -0600

    initial

---
+++ com/java/practice/ActivitySelectionGreedy.java
@@ -0,0 +1,15 @@
+package com.java.practice;
+import java.io.*;
+
+public class ActivitySelectionGreedy {
+
+public static int[]s = {1,3,0,5,8,5};
+public static int[]f = {2,4,6,7,9,9};
+public static int n=s.length;
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
